a46bc8ae522c907879f6a45037491f021d784bee
CDlee <80387658+CongDuongLe@users.noreply.github.com>
2022-12-18 14:37:27 +0700

    buoi1 jsi12

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomePage from './pages/HomePage'
+
+const App = () => {
+  return (
+    <div className='h-auto w-screen bg-blue-100'>
+        <HomePage />
+    </div>
+  )
+}
+
+export default App
