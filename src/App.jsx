d8c05ee223e5a632cb5875bb2102e59a1f7b411f
CDlee <80387658+CongDuongLe@users.noreply.github.com>
2023-02-12 14:05:35 +0700

    buoi3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Buoi2 from './pages/Buoi2'
+import Buoi3 from './pages/Buoi3'
 import HomePage from './pages/HomePage'
 
 const App = () => {
   return (
     <div className=' flex flex-1 h-auto min-h-screen w-screen bg-blue-100'>
         {/* <HomePage /> */}
-        <Buoi2 />
+        {/* <Buoi2 /> */}
+        <Buoi3 />
 
     </div>
   )
